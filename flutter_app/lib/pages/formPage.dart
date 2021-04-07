60bd2f71f4283b4da416d15c82d319ea6aeb0271
Iván <uo265368@uniovi.es>
2021-04-07 09:09:56 +0200

    keyboardTypes

--- flutter_app/lib/pages/formPage.dart
+++ flutter_app/lib/pages/formPage.dart
@@ -15,10 +15,14 @@ class _FormPageState extends State<FormPage> {
   //Guardar los valores del forms
   String name;
   String surname;
+  String email;
+  String phone;
 
   //Controladores de los datos del formulario para que no se pierdan (por ejemplo al deslizar en un listView como hago aquí)
   final nameTextController = TextEditingController();
   final surnameTextController = TextEditingController();
+  final emailTextController = TextEditingController();
+  final phoneTextController = TextEditingController();
 
   //Key global del forms
   final _formKey = GlobalKey<FormState>();
@@ -41,8 +45,21 @@ class _FormPageState extends State<FormPage> {
             child: Column(
               children: [
                 TextFormField(
+                  controller: emailTextController,
+                  decoration: InputDecoration(labelText: "Correo electrónico"),
+                  keyboardType: TextInputType.emailAddress,
+                  onSaved: (value) {
+                    email = value;
+                  }, validator: (value) {
+                  if(value.isEmpty){
+                    return "Campo vacío";
+                  }
+                },
+                ),
+                TextFormField(
                   controller: nameTextController,
                   decoration: InputDecoration(labelText: "Nombre"),
+                  keyboardType: TextInputType.name,
                   onSaved: (value) {
                     name = value;
                   }, validator: (value) {
@@ -54,9 +71,22 @@ class _FormPageState extends State<FormPage> {
                 TextFormField(
                   controller: surnameTextController,
                   decoration: InputDecoration(labelText: "Apellido"),
+                  keyboardType: TextInputType.name,
                     onSaved: (value) {
                       surname = value;
                     }, validator: (value) {
+                  if(value.isEmpty){
+                    return "Campo vacío";
+                  }
+                },
+                ),
+                TextFormField(
+                  controller: phoneTextController,
+                  decoration: InputDecoration(labelText: "Teléfono"),
+                  keyboardType: TextInputType.phone,
+                  onSaved: (value) {
+                    phone = value;
+                  }, validator: (value) {
                   if(value.isEmpty){
                     return "Campo vacío";
                   }
@@ -79,13 +109,15 @@ class _FormPageState extends State<FormPage> {
   void _send(){
     if(_formKey.currentState.validate()){
       _formKey.currentState.save();
-      Navigator.of(context).pushNamed("/list", arguments: FormData(this.name, this.surname),);
+      Navigator.of(context).pushNamed("/list", arguments: FormData(this.name, this.surname, this.email, this.phone),);
     }
   }
 }
 
 class FormData {
+  String email;
   String name;
   String surname;
-  FormData(this.name, this.surname);
+  String phone;
+  FormData(this.name, this.surname, this.email, this.phone);
 }
